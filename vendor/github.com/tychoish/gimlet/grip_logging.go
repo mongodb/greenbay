ea9c3fd1610375a49c8035bfaefed7fdef3b4e41
tycho garen <garen@tychoish.com>
2017-02-06 07:07:50 -0500

    MAKE-156: update vendored logging package, move vendored gimlet

---
+++ vendor/github.com/tychoish/gimlet/grip_logging.go
@@ -0,0 +1,42 @@
+package gimlet
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/tychoish/grip"
+	"github.com/tychoish/grip/logging"
+	"github.com/urfave/negroni"
+)
+
+// AppLogging provides a Negroni-compatible middleware to send all
+// logging using the grip packages logging. This defaults to using
+// systemd logging, but gracefully falls back to use go standard
+// library logging, with some additional helpers and configurations to
+// support configurable level-based logging. This particular
+// middlewear resembles the basic tracking provided by Negroni's
+// standard logging system.
+type AppLogging struct {
+	grip.Journaler
+}
+
+// NewAppLogger creates an logging middlear instance suitable for use
+// with Negroni. Sets the logging configuration to be the same as the
+// default global grip logging object.
+func NewAppLogger() *AppLogging {
+	l := &AppLogging{&logging.Grip{grip.GetSender()}}
+
+	return l
+}
+
+// Logs the request path, the beginning of every request as well as
+// the duration upon completion and the status of the response.
+func (l *AppLogging) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	start := time.Now()
+	l.Infof("Started %s %s", r.Method, r.URL.Path)
+
+	next(rw, r)
+
+	res := rw.(negroni.ResponseWriter)
+	l.Infof("Completed %v %s in %v", res.Status(), http.StatusText(res.Status()), time.Since(start))
+}
